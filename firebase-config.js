4f71f05dfe1cd62469d1b8986ede622ec71f45fe
Rishav Yadav <83490214+Rishav-Yadav@users.noreply.github.com>
2026-05-31 20:34:54 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,8 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyA_j_du12dLn2JXnkcR6x0yNT7iebhGZSk",
-    authDomain: "engineeringnotes-d530a.firebaseapp.com",
-    projectId: "engineeringnotes-d530a",
-    storageBucket: "engineeringnotes-d530a.firebasestorage.app",
-    messagingSenderId: "612162257887",
-    appId: "1:612162257887:web:c59c2adccda6bc5b547a6a"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const firebaseConfig = {
+  apiKey: "AIzaSyA_j_du12dLn2JXnkcR6x0yNT7iebhGZSk",
+  authDomain: "engineeringnotes-d530a.firebaseapp.com",
+  projectId: "engineeringnotes-d530a",
+  storageBucket: "engineeringnotes-d530a.firebasestorage.app",
+  messagingSenderId: "612162257887",
+  appId: "1:612162257887:web:c59c2adccda6bc5b547a6a"
+};
